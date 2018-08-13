7fd0c4729c4628cd8b213816b39522aa56dff349
Balagovind <balagovind2094@gmail.com>
2018-08-13 12:30:03 +0530

    RED: Initial cell postion test written

--- test_GoL.py
+++ test_GoL.py
@@ -9,3 +9,12 @@ def test_return():
     #print(grid_out[0][1])
     assert game_of_life.grid(grid) == grid_out
 
+def test_cell_position():
+    grid_size = 4
+    grid_matrix = game_of_life.grid(grid_size)
+    initial_positions = [['+', '+', '+', '+'],
+                         ['+', '1', '+', '+'],
+                         ['+', '1', '+', '+'],
+                         ['+', '1', '+', '+']]
+    
+    assert game_of_life.initial_position() == initial_position
