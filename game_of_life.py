c5a93b3f7c8f1acd1c2044fa48abbce78b504a54
Balagovind <balagovind2094@gmail.com>
2018-08-13 16:42:02 +0530

    RED: All tests passed, output not correct

--- game_of_life.py
+++ game_of_life.py
@@ -1,19 +1,15 @@
+import pprint
 def grid(n):
-    row = []
-    for i in range(n):
-        row.append(0)
     table = []
     for i in range(n):
+        row = [0]*n
         table.append(row)
-
+        
     return table
 
 
 def initial_position():
-    pos =  [[0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0],
-            [0, 0, 0, 0]]
+    pos =  grid(4)
     pos[0][1] = 0
     pos[1][1] = 1
     pos[2][1] = 1
@@ -23,34 +19,49 @@ def initial_position():
 
     
 def sum_of(i,j, new_position):
-    #new_position = initial_position()
     total = 0
     N = len(new_position)
-    #return new_position[i][j]
     total = new_position[(i-1)%N][(j-1)%N] + new_position[(i-1)%N][j] + new_position[(i-1)%N][(j+1)%N] + new_position[i][j-1] +  new_position[i][(j+1)%N] + new_position[(i+1)%N][(j-1)%N] + new_position[(i+1)%N][j] + new_position[(i+1)%N][(j+1)%N]
     return total
 
-def if_alive(i, j, new_position):
-    #new_position = initial_position()    
+def if_alive(i, j, new_position):    
     sum_ = sum_of(i,j, new_position)
     if sum_< 2 or sum_ >3:
+        #print(sum_)
         new_position[i][j] = 0
-        return new_position
-    else:
+        #print("if alive  ", new_position)
+        return new_position[i][j]
+    elif sum_==2 or sum_==3:
         new_position[i][j] = 1
-        return new_position
+        return new_position[i][j]
 
-def if_dead(i, j, new_position):
-    #new_position = initial_position()    
+def if_dead(i, j, new_position):    
     sum_ = sum_of(i,j, new_position)
     if sum_ == 3:
         new_position[i][j] = 1
-        return new_position
+        return new_position[i][j]
     else:
         new_position[i][j] = 0
-        return new_position
+        return new_position[i][j]
 
-# def main():
-#     new_position = initial_position()
-#     while True:
-        
+def main():
+    first_position = initial_position()
+    current_position = initial_position()
+    #print(new_position)
+    for x in range(2):
+        for i in range(4):
+            for j in range(4):
+                print(f"{i}, {j}")
+                if first_position[i][j] == 1:
+                    print(first_position)
+                    current_position[i][j] = if_alive(i, j, first_position[:])
+                    print(current_position)
+                else:
+                    print(first_position)
+                    current_position[i][j] = if_dead(i, j, first_position)
+                    print(current_position)
+
+        first_position = current_position
+        #print(new_position)
+        if x ==1:
+            exit()
